97bea2b60c75de066f53dbc35c666e60edea9abe
scubanet01-css <scubanet01@gmail.com>
2026-02-16 18:43:11 +0900

    dist app assets

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // ✅ 빌드 산출물(js/css) 폴더명을 바꿔서
+  // /assets/vessels (업로드 이미지)와 충돌 방지
+  build: {
+    assetsDir: "app-assets",
+  },
+
   server: {
     proxy: {
       // '/data': 'http://localhost:3002'
-    }
-  },
-  build: {
-    assetsDir: "app-assets",
+    },
   },
 });
