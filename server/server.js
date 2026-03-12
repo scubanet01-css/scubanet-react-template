b866aecbe301fcf984f268b535f44c1255d3144e
scubanet01-css <scubanet01@gmail.com>
2026-03-12 21:05:06 +0900

    재재수정

--- server/server.js
+++ server/server.js
@@ -37,7 +37,15 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "http://210.114.22.82:4002");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(bodyParser.json());
