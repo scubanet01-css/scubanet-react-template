e891f60de7b31ef396774d39876a5f5dcef114e5
scubanet01-css <scubanet01@gmail.com>
2026-03-21 17:21:51 +0900

    Footer 저장

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -11,39 +11,39 @@ function Footer() {
             <div className="footer-section sitemap">
                 <h3>사이트맵</h3>
                 <ul>
-                    <li><Link to="/list">여행목록</Link></li>
-                    <li><Link to="/instructor/list">강사프로그램</Link></li>
-                    <li><Link to="/special">스쿠버넷 투어</Link></li>
-                    <li><Link to="/mybooking">내 예약</Link></li>
-                    <li><Link to="/login">로그인</Link></li>
+                    <li><Link to="/triplist">여행목록</Link></li>
+                    <li><Link to="/specialtrips">스페셜트립</Link></li>
+                    <li><Link to="/booking/summary">내예약</Link></li>
+                    <li><Link to="/auth/login">로그인</Link></li>
                 </ul>
             </div>
 
             {/* 회사 정보 */}
             <div className="footer-section company-info">
-                <h3>스쿠버넷 여행사</h3>
-                <p>대표: 최성순</p>
-                <p>사업자등록번호: 000-00-00000</p>
-                <p>주소: 서울시 ○○구 ○○로 123</p>
-                <p>이메일: scubanet@scubanet.kr</p>
-                <p>전화번호: 02-1234-5678</p>
+                <h3>(주)스쿠버넷트레블</h3>
+                <p>사업자등록번호: 230-81-02-952</p>
+                <p>주소: 경기도 화성시 동탄영천로 101, 서영아너시티 1014호</p>
+                <p>
+                    이메일:{" "}
+                    <a href="mailto:info@scubanet-travel.com">
+                        info@scubanet-travel.com
+                    </a>
+                </p>
+                <p>카카오톡: scubanet</p>
             </div>
 
-            {/* 증서 이미지 */}
+            {/* 증서 / 등록증 */}
             <div className="footer-section certificates">
                 <h3>증서 / 등록증</h3>
-                <div className="cert-image-wrapper">
-                    <img src="/images/certificates/travel-license.jpg" alt="여행업 등록증" />
-                </div>
-
-                <div className="cert-image-wrapper">
-                    <img src="/images/certificates/insurance.jpg" alt="해외여행 보증보험증서" />
-                </div>
+                <ul className="certificate-list">
+                    <li>여행업등록증</li>
+                    <li>해외여행 보증보험증서</li>
+                </ul>
             </div>
 
             {/* 카피라이트 */}
             <div className="footer-bottom">
-                © 2025 ScubaNet Travel. All rights reserved.
+                © 2026 ScubaNet Travel. All rights reserved.
             </div>
 
         </footer>
